e253a0ee7b2c5dd04f9513c4b0cb642bdef83a5f
EkaterinaAsanova <u-kat@mail.ru>
2025-12-17 14:10:28 +0500

    Восьмое задание полностью готово

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,54 @@
-//Задача № 1
+//const md5 = require('js-md5');
+
 function cachingDecoratorNew(func) {
-  
+	let cache = [];
+
+	function wrapper(...args) {
+		const hash = md5(JSON.stringify(args));
+		let objectInCache = cache.find(item => item.hash === hash);
+		if (objectInCache) {
+			console.log("Из кеша: ", objectInCache.value, cache);
+			return "Из кеша: " + objectInCache.value;
+
+		}
+		let result = func(...args);
+		cache.push({
+			hash: hash,
+			value: result
+		});
+
+		if (cache.length > 5) {
+			cache.shift();
+		}
+		console.log("Вычисляем: ", +result, cache);
+		return "Вычисляем: " + result;
+	}
+	return wrapper;
 }
 
-//Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+	let timeoutId = null;
+	wrapper.count = 0;
+	wrapper.allCount = 0;
+
+	function wrapper(...args) {
+		wrapper.allCount++;
+		if (timeoutId) {
+			console.log('Уже есть таймаут', args);
+			clearTimeout(timeoutId);
+		}
+		if (!wrapper.count) {
+			console.log('первый сигнал', args);
+			func.call(this, ...args);
+			wrapper.count++;
+		}
+		timeoutId = setTimeout(() => {
+			console.log('задержка больше 200 млсек, сработал таймаут');
+			func.apply(this, args);
+			clearTimeout(timeoutId);
+			timeoutId = null;
+			wrapper.count++;
+		}, delay);
+	}
+	return wrapper;
 }
